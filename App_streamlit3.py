9645a76d35b7aef458c94db12b3abc20ebe0e798
Operaciones1234 <martinakozameh61@gmail.com>
2026-02-13 19:54:38 -0300

    Update App_streamlit3.py

--- App_streamlit3.py
+++ App_streamlit3.py
@@ -11,7 +11,8 @@ from urllib.parse import quote
 # =============================================================================
 # 1. IMPORTACIONES
 # =============================================================================
-from Routing_logic3 import (COORDENADAS_LOTES, solve_route_optimization, VEHICLES, COORDENADAS_ORIGEN,
+from Routing_logic3 import (
+COORDENADAS_LOTES, solve_route_optimization, VEHICLES, COORDENADAS_ORIGEN,
     generate_geojson_io_link, generate_geojson, COORDENADAS_LOTES_REVERSO
 )
 
@@ -380,4 +381,5 @@ elif page == "Estadísticas":
             )
     else:
         st.info("Se requieren datos operativos para generar los indicadores.")
+
 
